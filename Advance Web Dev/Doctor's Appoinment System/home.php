c19a6721c0d76f7301850c23040abd2c974e7007
MD. Fazle Rabbi Riyad <135745417+RiyadAndroidDev@users.noreply.github.com>
2024-05-25 23:30:40 +0600

    Starts new project Hospital Management System. In this push,there is only opening of all files with it's extentions

---
+++ Advance Web Dev/Doctor's Appoinment System/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Header Section Starts -->
+    <section class="Header">
+        <a href="home.php" class="logo">Your Doctors</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="doctor.php">Home</a>
+            <a href="Appoinrment.php">Appointment</a>
+        </nav>
+
+    </section>
+
+    <!-- Header Section Ends -->
+    <script rel="" <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
